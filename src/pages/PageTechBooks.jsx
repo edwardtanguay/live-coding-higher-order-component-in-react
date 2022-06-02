64ee0398518a62678e82df5c080ced909699e68c
Edward Tanguay (windows) <edward@tanguay.info>
2022-06-02 11:36:51 +0200

    both pages show books

--- src/pages/PageTechBooks.jsx
+++ src/pages/PageTechBooks.jsx
@@ -3,6 +3,11 @@ export const PageTechBooks = ({techBooks}) => {
 		<div className="page_techBooks">
 			<h2>Tech Books</h2>
 			<p>Here are the {techBooks.length} tech books.</p>
+			<ul>
+				{techBooks.map((book, index) => {
+					return <li key={index}>{book.title}</li>;
+				})}
+			</ul>
 		</div>
 	)
 }
